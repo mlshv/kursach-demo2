98f4257acb9726e75230454a2a60e83cc98bd153
Mikhail Malyshev <mlshv42@yandex.ru>
2022-01-29 16:12:54 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Привет, Курсач!</h1>
+      <h2>Как насчёт того, чтобы сделать что-то пиздатое?</h2>
+    </div>
+  );
+}
